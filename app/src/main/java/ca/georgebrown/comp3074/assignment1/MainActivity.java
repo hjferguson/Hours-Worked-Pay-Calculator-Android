0468b3223ff958a285a2660ab773125ec631cfd0
Harlan Ferguson <harlanjazz@gmail.com>
2023-10-16 11:59:29 -0400

    adds basic main layout

--- app/src/main/java/ca/georgebrown/comp3074/assignment1/MainActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/assignment1/MainActivity.java
@@ -3,8 +3,17 @@ package ca.georgebrown.comp3074.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView title;
+    EditText hoursWorked;
+    EditText hourlyRate;
+    Button calculateRate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
